8abf4e5b2b15b8c4bf2efbc732b90f7e41add6da
Anubhav Maity <anubhavmaity@Anubhavs-MacBook-Air.local>
2015-04-27 19:57:05 +0530

    bind scroll event to image

--- lazyload.js
+++ lazyload.js
@@ -4,15 +4,21 @@ var image_jsonData_length;
 var window_h = $(window).height();
 var count = 1;
 var image_height = 640;
+var id = "#image1";
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
+
     //user scrolls down the page enough to bring them inside the visible viewport region to load the subsequent image
-    if (scroll > window_h + (count - 1) * image_height - 450 && count < image_jsonData_length) {
-        var id = "#image" + count;
-        $(id).attr('src', image_jsonData[count].images.standard_resolution.url);
-        count = count + 1;
-    }   
+        $('img[realsrc]').each(function(i){
+          var t = $(this);
+          if(t.position().top < (scroll+$(window).height())){
+            t.attr('src', image_jsonData[count].images.standard_resolution.url); // trigger the image load
+            t.removeAttr('realsrc'); // so we only process this image once
+            count = count + 1;
+        }
+    
+    });
 });
 
 $.ajax({
@@ -26,10 +32,11 @@ $.ajax({
         for (x in data.data) {
             var id="image"+x
             //create placeholder of all images of same size
-            $('.page').append('<img class="lazy" id="'+id+'" width="640" height="640"> <br/>');
+            $('.page').append('<img class="lazy" id="'+id+'" realsrc="" width="640" height="640"> <br/>');
         }
         //load the first image
         $('#image0').attr('src', image_jsonData[0].images.standard_resolution.url);
+        $('#image0').removeAttr('realsrc');
 
     },
     error: function(data){
